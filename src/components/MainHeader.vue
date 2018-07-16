4c6e662347fbd7a3d392c324da07445811845f48
Jason Priem <jp@jasonpriem.org>
2018-07-16 12:32:25 -0700

    oscar logo.

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,13 +1,17 @@
 <template>
     <md-toolbar class="main-header">
         <div class="brands">
-            <h2>
-                Open Science Dashboard
-            </h2>
-            <h1>
-                National Institutes of Mental Health
-            </h1>
+            <a class="us" href="/">
+                <span class="main">OSCAR</span>
+                <span class="extra">
+                     open science compliance and assessment report
+                </span>
+            </a>
+            <a class="institution" href="./">National Institute of Mental Health</a>
 
+        </div>
+        <div class="nav">
+            <router-link to="./faq">FAQ</router-link>
         </div>
     </md-toolbar>
 
@@ -22,14 +26,44 @@
 </script>
 
 <style scoped lang="scss">
-    h2 {
-        margin: 20px 10px 10px 10px;
-        font-weight: 300;
-        font-size: 16px;
+    .brands {
+        flex: 1;
+        display: flex;
+        padding: 15px;
+        align-items: center;
+        a.us {
+            line-height: .8;
+            border-right: 1px solid;
+            padding: 0 10px 0 0;
+            text-align: right;
+            margin-right: 10px;
+            display: block;
+            width: 8.5em;
+            &:hover {
+                text-decoration: none;
+            }
+            .main {
+                font-size: 32px;
+                display: block;
+            }
+            .extra {
+                font-size: 11px;
+
+            }
+        }
+        .institution {
+            font-size: 18px;
+            color: #333;
+            max-width: 10em;
+        }
     }
-    h1 {
-        margin: 10px;
-        margin-bottom: 20px;
+    .nav {
+        margin-right: 10px;
+        a {
+            font-size: 18px;
+            padding: 5px;
+            display: block;
+        }
     }
 
 
